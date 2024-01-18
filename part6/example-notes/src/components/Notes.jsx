2a6447cf84a66870f2931ae21c1b36331e8f79ea
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2024-01-18 14:24:42 +0200

    Added example projects

---
+++ part6/example-notes/src/components/Notes.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from "react-redux"
+import { toggleImportanceOf } from "../reducers/noteReducer"
+
+const Note = ({ note, handleClick }) => {
+    return (
+        <li onClick={handleClick}>
+            {note.content}
+            <strong>{note.important ? ' important' : ''}</strong>
+        </li>
+    )
+}
+
+const Notes = () => {
+    const notes = useSelector(({ filter, notes }) => {
+        if (filter === 'ALL') return notes
+        return filter === 'IMPORTANT'
+            ? notes.filter(n => n.important)
+            : notes.filter(n => !n.important)
+    })
+    const dispatch = useDispatch()
+
+    return (
+        <ul>
+            {notes.map(note =>
+                <Note
+                    key={note.id}
+                    note={note}
+                    handleClick={() => dispatch(toggleImportanceOf(note.id))}
+                />
+            )}
+        </ul>
+    )
+}
+
+export default Notes
