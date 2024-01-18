b8f06c071e719674df336efc6f86d32a665932b2
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2024-01-18 11:49:15 +0200

    6.22 done

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,26 +1,37 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
 
   const result = useQuery({
     queryKey: ['anecdotes'],
-    queryFn: getAnecdotes
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false
   })
+
   console.log(JSON.parse(JSON.stringify(result)))
 
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.map(a => a.id === updatedAnecdote.id ? updatedAnecdote : a).sort((a1, a2) => a2.votes - a1.votes))
+    }
+  })
+
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
   }
 
   if (result.isLoading) return <div>loading data...</div>
 
   if (result.isError) return <div>anecdote service not available due to problems in server</div>
 
-  const anecdotes = result.data
+  const anecdotes = result.data.sort((a1, a2) => a2.votes - a1.votes)
 
   return (
     <div>
