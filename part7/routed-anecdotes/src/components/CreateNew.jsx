a404a36e81997d3b5c48fadfe499fde3cad2c7d3
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2024-01-19 15:26:22 +0200

    7.4 done

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,25 +1,25 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
 
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
 
         navigate('/')
 
-        props.setNotification(`a new anecdote ${content} created`)
+        props.setNotification(`a new anecdote ${content.value} created`)
         setTimeout(() => {
             props.setNotification('')
         }, 5000)
@@ -30,16 +30,13 @@ const CreateNew = (props) => {
             <h2>create a new anecdote</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    content
-                    <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+                    content <input {...content} />
                 </div>
                 <div>
-                    author
-                    <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    author <input {...author} />
                 </div>
                 <div>
-                    url for more info
-                    <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+                    url for more info <input {...info} />
                 </div>
                 <button>create</button>
             </form>
