399a5c80a7c18504906a0922481b43f6d3646bc8
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2024-02-14 13:51:02 +0200

    9.12 done

--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import patients from '../../data/patients';
-import { NoSsnPatient } from '../types';
+import { Patient, NoSsnPatient, NewPatient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const getNoSsnPatients = (): NoSsnPatient[] => {
     return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -11,6 +12,19 @@ const getNoSsnPatients = (): NoSsnPatient[] => {
     }));
 };
 
+const addPatient = (patient: NewPatient): Patient => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const id = String(uuid());
+    const newPatient = {
+        id: id,
+        ...patient
+    };
+
+    patients.push(newPatient);
+    return newPatient;
+};
+
 export default {
-    getNoSsnPatients
+    getNoSsnPatients,
+    addPatient
 };
